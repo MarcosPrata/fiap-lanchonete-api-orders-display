bf540bcd1f22f839ba4e1cfdb6cc5c89d5c5d2ed
Marcos Prata <mhpjunior3@gmail.com>
2023-06-14 19:23:35 -0300

    Initial commit

---
+++ src/main/kotlin/com/soat220/lanchonete/LanchoneteApplication.kt
@@ -0,0 +1,11 @@
+package com.soat220.lanchonete
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LanchoneteApplication
+
+fun main(args: Array<String>) {
+	runApplication<LanchoneteApplication>(*args)
+}
