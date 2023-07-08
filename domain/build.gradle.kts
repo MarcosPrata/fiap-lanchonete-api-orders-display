56f44c7138c44c5c1ab5eb4aa46464a2c48e29de
Marcos Prata <mhpjunior3@gmail.com>
2023-07-08 04:44:41 -0300

    Ajustar Dockerfile e docker-compose.yml

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -12,12 +12,12 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("javax.inject:javax.inject:1")
-    implementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    testImplementation("org.junit.vintage:junit-vintage-engine:5.7.1")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
-    testImplementation("org.mockito:mockito-inline:4.3.1")
+    implementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("org.junit.vintage:junit-vintage-engine:5.9.2")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
+    testImplementation("org.mockito:mockito-inline:5.2.0")
     testImplementation("io.mockk:mockk:1.13.4")
 }
 
