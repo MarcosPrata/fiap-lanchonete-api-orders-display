eb95afb9c904de71719ab347def791d604ce7537
Marcos Prata <mhpjunior3@gmail.com>
2023-11-11 14:58:31 -0300

    Adjust pipe #24 Adjust ci/cd

---
+++ application/src/test/kotlin/com/soat220/lanchonete/unit/UnitTest.kt
@@ -0,0 +1,13 @@
+package com.soat220.lanchonete.unit
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class UnitTest {
+    @Test
+    fun testItWorks() {
+        val fooString = "foo"
+
+        Assertions.assertEquals("foo", fooString)
+    }
+}
